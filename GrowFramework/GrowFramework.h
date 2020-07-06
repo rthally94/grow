8e5843ebb433b8ccff097e9cf90df1f26c0eb661
Ryan Thally <ryan.thally@gmail.com>
2020-07-06 12:44:14 -0400

    Initial Commit

---
+++ GrowFramework/GrowFramework.h
@@ -0,0 +1,19 @@
+//
+//  GrowFramework.h
+//  GrowFramework
+//
+//  Created by Ryan Thally on 7/6/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GrowFramework.
+FOUNDATION_EXPORT double GrowFrameworkVersionNumber;
+
+//! Project version string for GrowFramework.
+FOUNDATION_EXPORT const unsigned char GrowFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrowFramework/PublicHeader.h>
+
+
