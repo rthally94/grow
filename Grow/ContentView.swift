2cbdf0f6e3dd3c392efea846854e831f764d0ba0
Ryan Thally <ryan.thally@gmail.com>
2020-07-08 17:11:40 -0400

    Initial Commit

---
+++ Grow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grow
+//
+//  Created by Ryan Thally on 7/8/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
