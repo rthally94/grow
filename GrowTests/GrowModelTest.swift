cbd7e079664b1808b752c26437205e144f4853d0
Ryan Thally <ryan.thally@gmail.com>
2020-07-08 20:18:04 -0400

    Initial Commit

---
+++ GrowTests/GrowModelTest.swift
@@ -0,0 +1,16 @@
+//
+//  PlantModelTest.swift
+//  GrowFrameworkTests
+//
+//  Created by Ryan Thally on 7/6/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import GrowFramework
+
+class GrowModelTest: XCTestCase {
+    
+}
