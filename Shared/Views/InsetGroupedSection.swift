38392303732058ad07dbcce1bb34d7e34441ebc7
Ryan Thally <ryan.thally@gmail.com>
2020-07-14 13:41:46 -0400

    Moved general views to own folder

---
+++ Shared/Views/InsetGroupedSection.swift
@@ -0,0 +1,21 @@
+//
+//  InsetGroupedSection.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/14/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct InsetGroupedSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InsetGroupedSection_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetGroupedSection()
+    }
+}
---
+++ Shared/Views/StatCell.swift
@@ -0,0 +1,21 @@
+//
+//  StatCell.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/14/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct StatCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatCell_Previews: PreviewProvider {
+    static var previews: some View {
+        StatCell()
+    }
+}
