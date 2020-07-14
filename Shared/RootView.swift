a0e6ce2f9b0c1c2d38a266c9f0ec4430f208e609
Ryan Thally <ryan.thally@gmail.com>
2020-07-14 11:43:23 -0400

    Added to manage tab view

--- Shared/RootView.swift
+++ Shared/RootView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            AllPlantsView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.dash")
+                        Text("Plants")
+                    }
+            }
+        }
     }
 }
 
