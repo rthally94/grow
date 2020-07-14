096d6d2c40fef26c1a2b095dd5e0d8d25e3fba66
Ryan Thally <ryan.thally@gmail.com>
2020-07-14 13:49:00 -0400

    Created Plant Editor

---
+++ Shared/Plant Editor/Plant Editor.swift
@@ -0,0 +1,21 @@
+//
+//  Plant Editor.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/14/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct Plant_Editor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Plant_Editor_Previews: PreviewProvider {
+    static var previews: some View {
+        Plant_Editor()
+    }
+}
