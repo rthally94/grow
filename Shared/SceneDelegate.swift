0d4b3fa1003e97f32235e41ac67303e63812d66f
Ryan Thally <ryan.thally@gmail.com>
2020-07-14 11:43:53 -0400

    Updated to use RootView as starting point

--- Shared/SceneDelegate.swift
+++ Shared/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
         let model = GrowModel()
-        let contentView = AllPlantsView()
+        let contentView = RootView()
             .environmentObject(model)
             .environment(\.managedObjectContext, context)            
 
