7e790b258c7f0d76462e5b23d99156bf171819a5
Ryan Thally <ryan.thally@gmail.com>
2020-07-15 08:11:20 -0400

    Removed old UIKIt code, added SwiftUI WeekPicker

---
+++ Shared/Plant Editor/PlantEditorForm.swift
@@ -0,0 +1,21 @@
+//
+//  PlantEditorForm.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/15/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlantEditorForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlantEditorForm_Previews: PreviewProvider {
+    static var previews: some View {
+        PlantEditorForm()
+    }
+}
---
+++ Shared/Views/WeekPicker.swift
@@ -0,0 +1,21 @@
+//
+//  WeekPicker.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/15/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct WeekPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeekPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        WeekPicker()
+    }
+}
