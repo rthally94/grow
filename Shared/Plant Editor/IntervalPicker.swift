3b74a406a5a85ce4da5e15bfe0b9148ed9bece59
Ryan Thally <ryan.thally@gmail.com>
2020-07-16 20:08:14 -0400

    Implemented IntervalPicker

--- Shared/Plant Editor/IntervalPicker.swift
+++ Shared/Plant Editor/IntervalPicker.swift
@@ -9,13 +9,61 @@
 import SwiftUI
 
 struct IntervalPicker: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var onSave: (CareInterval) -> Void
+    
+    @State private var unitChoice = CareInterval.Unit.daily
+    @State private var weekdayChoices = Set(arrayLiteral: 0)
+    @State private var startDayChoice = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                ForEach(CareInterval.Unit.allCases, id: \.self) { unit in
+                    HStack {
+                        Text(unit.description)
+                        Spacer()
+                        if self.unitChoice == unit {
+                            Image(systemName: "checkmark")
+                                .transition(.identity)
+                        }
+                    }
+                    .onTapGesture {
+                        withAnimation {
+                            self.unitChoice = unit
+                        }
+                    }
+                }
+            }
+            
+            if unitChoice == .weekly {
+                Section {
+                    WeekPicker(selection: $weekdayChoices)
+                }
+            }
+            
+            if unitChoice == .monthly {
+                Section {
+                    DatePicker("Starting on", selection: $startDayChoice, in: startDate...endDate, displayedComponents: .date)
+                }
+            }
+        }
+        .listStyle(GroupedListStyle())
+        .navigationBarTitle("Inerval Picker", displayMode: .inline)
     }
+    
+    // MARK: Constants
+    private let startDate = Date()
+    private let endDate = Calendar.current.date(byAdding: .year, value: 10, to: Date()) ?? Date()
 }
 
 struct IntervalPicker_Previews: PreviewProvider {
     static var previews: some View {
-        IntervalPicker()
+        NavigationView {
+            IntervalPicker { interval in
+                print(interval)
+            }
+        }
     }
 }
