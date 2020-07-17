bc31f2c6d23558a280ecb3ce2309cffbdd33f871
Ryan Thally <ryan.thally@gmail.com>
2020-07-16 20:03:08 -0400

    Created Files

---
+++ Shared/Model/CareInterval.swift
@@ -0,0 +1,9 @@
+//
+//  CareInterval.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/16/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import Foundation
---
+++ Shared/Plant Editor/IntervalPicker.swift
@@ -0,0 +1,21 @@
+//
+//  IntervalPicker.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/16/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct IntervalPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntervalPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        IntervalPicker()
+    }
+}
