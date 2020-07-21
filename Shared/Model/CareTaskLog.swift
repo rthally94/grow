15ed3ddb8e9151488f0a2478887b806bbe81be3b
Ryan Thally <ryan.thally@gmail.com>
2020-07-21 11:25:43 -0400

    Refactoring

--- Shared/Model/CareTaskLog.swift
+++ Shared/Model/CareTaskLog.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct CareActivity: Identifiable, Hashable {
+struct CareTaskLog: Identifiable, Hashable {
     enum CareType {
         case water, prune, fertilize
         
---
+++ Shared/Plant Editor/CareTaskEditor/CareTaskEditor.swift
@@ -0,0 +1,21 @@
+//
+//  CareTaskEditor.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/19/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct CareTaskEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CareTaskEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        CareTaskEditor()
+    }
+}
