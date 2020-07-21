40375a5853cf10aa84f61678f1c3b0c367aad390
Ryan Thally <ryan.thally@gmail.com>
2020-07-21 11:29:00 -0400

    Implemented care task
    Model allows for adding multiple care tasks by the user.

--- Shared/Model/CareTask.swift
+++ Shared/Model/CareTask.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+struct CareTask: Identifiable, Hashable, Equatable {
+    let id: UUID
+    var name: String
+    var interval: CareInterval
+    
+    var notes: String
+    
+    var logs: [CareTaskLog]
+    
+    // MARK: Initializers
+    
+    init() {
+        self.init(id: UUID(), name: "New Task", interval: .init(), notes: "", logs: [])
+    }
+    
+    init(id: UUID, name: String, interval: CareInterval, notes: String, logs: [CareTaskLog]) {
+        self.id = id
+        self.name = name
+        self.interval = interval
+        self.notes = notes
+        self.logs = logs
+    }
+}
+
+// MARK: Intents
+extension CareTask {
+        mutating func addLog(_ log: CareTaskLog) {
+            if let index = logs.firstIndex(where: { $0.id == log.id }) {
+                logs[index] = log
+            } else {
+                logs.insert(log, at: 0)
+            }
+        }
+    
+        var latestLog: CareTaskLog? {
+            return logs.first
+        }
+}
--- Shared/Model/Plant.swift
+++ Shared/Model/Plant.swift
@@ -15,63 +15,47 @@ struct Plant: Identifiable, Hashable, Equatable {
     var pottingDate: Date?
     
     // Care Info
-    var sunTolerance: SunTolerance
-    var wateringInterval: CareInterval
-    
-    var age: TimeInterval {
-        return DateInterval(start: pottingDate ?? Date(), end: Date()).duration
-    }
+    var careTasks: [CareTask]
     
     // Care Activity
-    private(set) var careActivity = [CareActivity]()
+//    private(set) var careActivity = [CareTaskLog]()
     
     // MARK: Initializers
     init() {
-        self.init(id: UUID(), name: "My Plant", pottingDate: nil, sunTolerance: .fullShade, wateringInterval: CareInterval())
+        self.init(id: UUID(), name: "My Plant", pottingDate: nil, careTasks: [])
     }
     
     init(name: String) {
-        self.init(id: UUID(), name: name, pottingDate: nil, sunTolerance: .fullShade, wateringInterval: CareInterval())
+        self.init(id: UUID(), name: name, pottingDate: nil, careTasks: [])
     }
     
-    init(id: UUID, name: String, pottingDate: Date?, sunTolerance: SunTolerance, wateringInterval: CareInterval) {
+    init(id: UUID, name: String, pottingDate: Date?, careTasks: [CareTask]) {
         self.id = id
         self.name = name
         self.pottingDate = pottingDate
-        self.sunTolerance = sunTolerance
-        self.wateringInterval = wateringInterval
+        self.careTasks = careTasks
+    }
+}
+
+extension Plant {
+    var age: TimeInterval {
+        return DateInterval(start: pottingDate ?? Date(), end: Date()).duration
     }
 }
 
 // MARK: Plant Intents
 extension Plant {
-    mutating func addCareActivity(_ log: CareActivity) {
-        careActivity.insert(log, at: 0)
-    }
-    
-    var latestWaterActivity: CareActivity? {
-        return getWaterActivity().first
-    }
-    
-    func getWaterActivity(max: Int = 1) -> [CareActivity] {
-        return getActivity(for: .water, max: max)
-    }
-    
-    func getActivity(for type: CareActivity.CareType? = nil, max: Int = 1) -> [CareActivity] {
-        var logs = [CareActivity]()
-        
-        if let logType = type {
-            for activity in careActivity where activity.type == logType {
-                logs.append(activity)
-                if logs.count == max { break }
-            }
+    mutating func addCareTask(_ task: CareTask) {
+        if let index = careTasks.firstIndex(where: { $0.id == task.id }) {
+            careTasks[index] = task
         } else {
-            for activity in careActivity {
-                logs.append(activity)
-                if logs.count == max { break }
-            }
+            careTasks.append(task)
         }
-        
-        return logs
+    }
+    
+    mutating func removeCareTask(_ task: CareTask) {
+        if let index = careTasks.firstIndex(of: task) {
+            careTasks.remove(at: index)
+        }
     }
 }
