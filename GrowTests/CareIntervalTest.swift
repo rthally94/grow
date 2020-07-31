948ccc3e3406164c6c73c4c7e607aaf245cd7706
Ryan Thally <ryan.thally@gmail.com>
2020-07-30 22:20:19 -0400

    Updated to use "Set" for interval

--- GrowTests/CareIntervalTest.swift
+++ GrowTests/CareIntervalTest.swift
@@ -15,21 +15,10 @@ class CareIntervalTest: XCTestCase {
     let cal = Calendar.current
     
     // MARK: Interval
-    func testCareInterval_WhenIntervalIsGreatorThanZero_IntervalIsGreatorThanZero() {
+    func testCareInterval_WhenUnitIsDaily_IntervalOnlyContainsOneValue() {
         let interval = 5
-        let sut = CareInterval(unit: .daily, interval: interval)
-        XCTAssertEqual(sut.interval, interval)
-    }
-    
-    func testCareInterval_WhenIntervalIsZero_IntervalIsZero() {
-        let interval = 0
-        let sut = CareInterval(unit: .daily, interval: interval)
-        XCTAssertEqual(sut.interval, interval)
-    }
-    
-    func testCareInterval_WhenIntervalIsLessThanZero_IntervalIsZero() {
-        let sut = CareInterval(unit: .daily, interval: -5)
-        XCTAssertEqual(sut.interval, 0)
+        let sut = CareInterval(unit: .daily, interval: [interval, interval+1])
+        XCTAssertEqual(sut.interval.count, 1)
     }
     
     // MARK: Unit Interval Boundary
@@ -40,8 +29,8 @@ class CareIntervalTest: XCTestCase {
     }
     
     func test_CareInterval_WhenUnitIsNone_IntervalIsZero() {
-        let sut = CareInterval(unit: .never, interval: 4)
-        XCTAssertEqual(sut.interval, 0)
+        let sut = CareInterval(unit: .never, interval: [4])
+        XCTAssertTrue(sut.interval.isEmpty)
     }
     
     func testCareInterval_WhenUnitIsDaily_IntervalIsOne() {
@@ -87,7 +76,7 @@ class CareIntervalTest: XCTestCase {
     
     // MARK: Next Date
     func testCareInterval_WhenUnitIsDaily_NextDateIsTomorrow() {
-        let sut = CareInterval(unit: .daily, interval: 1)
+        let sut = CareInterval(unit: .daily, interval: [1])
 
         let today = Date()
         let next = sut.next(from: today)
