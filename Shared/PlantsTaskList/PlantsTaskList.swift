c9a186f6c10303226deb90ef38d077ee92c05dac
Ryan Thally <ryan.thally@gmail.com>
2020-07-30 21:52:36 -0400

    Initial Commit

---
+++ Shared/PlantsTaskList/PlantsTaskList.swift
@@ -0,0 +1,21 @@
+//
+//  PlantsTaskList.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 7/28/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlantsTaskList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlantsTaskList_Previews: PreviewProvider {
+    static var previews: some View {
+        PlantsTaskList()
+    }
+}
