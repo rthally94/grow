44eca90ae11b331d94109772f854f473cc9d31ea
Ryan Thally <ryan.thally@gmail.com>
2020-07-31 21:49:29 -0400

    Updated for better reusability

--- Shared/Views/UICollectionViewWrapper.swift
+++ Shared/Views/UICollectionViewWrapper.swift
@@ -8,122 +8,101 @@
 
 import SwiftUI
 
-struct UICollectionViewWrapper<Section, ItemIdentifier: Hashable, Header: View, Content: View>: UIViewRepresentable where Section: Hashable, Section: Comparable {
-    let cellReuseIdentifier = "CollectionViewCell"
-    let headerReuseIdentifier = "CollectionViewHeader"
+struct CompositionalCollection<ItemIdentifier: Hashable, Content: View>: UIViewControllerRepresentable {
+    var sections: [CompositionalCollectionSection<ItemIdentifier>]
+    var content: (IndexPath, ItemIdentifier) -> Content
     
-    var data: [Section: [ItemIdentifier] ]
-    var boundarySupplementaryItem: (_ section: Section, _ kind: String) -> Header
-    var content: (ItemIdentifier) -> Content
-    
-    func makeUIView(context: Context) -> UICollectionView {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: context.coordinator.configureLayout())
-    
-        collectionView.register(CollectionViewCell<Content>.self, forCellWithReuseIdentifier: cellReuseIdentifier)
-        collectionView.register(CollectionViewSectionHeaderFooter<Header>.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerReuseIdentifier)
-//        let dataSource = UICollectionViewDiffableDataSource<Section, ItemIdentifier>(collectionView: collectionView) { (collectionView, indexPath, item) -> UICollectionViewCell? in
-//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TestCell.reuseID, for: indexPath) as? TestCell
-////            let view = self.content(indexPath)
-////            cell?.configure(with: { view })
-//            return cell
-//        }
+    func makeUIViewController(context: Context) -> UICollectionViewController {
+        let collectionViewController = UICollectionViewController(collectionViewLayout: makeLayout())
         
-        collectionView.dataSource = context.coordinator
-//        updateDataSource(dataSource)
-//        context.coordinator.dataSource = dataSource
+        collectionViewController.collectionView.register(CompositionalCollectionCell<Content>.self, forCellWithReuseIdentifier: CompositionalCollectionCell<Content>.reuseIdentifier)
         
-        collectionView.delegate = context.coordinator
-        collectionView.backgroundColor = .clear
+        let dataSource = makeDataSource(collectionView: collectionViewController.collectionView)
+        context.coordinator.dataSource = dataSource
         
-        return collectionView
-    }
-    
-    func updateUIView(_ uiView: UICollectionView, context: Context) {
-//        guard let dataSource = context.coordinator.dataSource else { return }
-//        updateDataSource(dataSource)
-    }
-    
-    func updateDataSource(_ dataSource: UICollectionViewDiffableDataSource<Section, ItemIdentifier>) {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, ItemIdentifier>()
-        snapshot.appendSections(Array(data.keys).sorted())
-        for key in data.keys {
-            if let values = data[key] {
-                snapshot.appendItems(values, toSection: key)
-            }
+        var snapshot = NSDiffableDataSourceSnapshot<CompositionalCollectionSection<ItemIdentifier>, ItemIdentifier>()
+        snapshot.appendSections(sections)
+        for section in sections {
+            snapshot.appendItems(section.items, toSection: section)
         }
         
         dataSource.apply(snapshot)
+        
+        collectionViewController.collectionView.backgroundColor = .clear
+        collectionViewController.navigationItem.title = "CCV!"
+        
+        return collectionViewController
     }
     
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
+    func updateUIViewController(_ uiViewController: UICollectionViewController, context: Context) {
+        print("Bla")
+        var snapshot = NSDiffableDataSourceSnapshot<CompositionalCollectionSection<ItemIdentifier>, ItemIdentifier>()
+        snapshot.appendSections(sections)
+        for section in sections {
+            snapshot.appendItems(section.items, toSection: section)
+        }
+        
+        context.coordinator.dataSource?.apply(snapshot)
     }
     
-    class Coordinator: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
-        func numberOfSections(in collectionView: UICollectionView) -> Int {
-            parent.data.keys.count
+    func makeLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (section, layoutEnvirontment) -> NSCollectionLayoutSection? in
+            return self.sections[section].layout()
         }
         
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            let sectionKey = Array(parent.data.keys)[section]
-            return parent.data[sectionKey]?.count ?? 0
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: parent.cellReuseIdentifier, for: indexPath) as? CollectionViewCell<Content> else { fatalError() }
-            let section = Array(parent.data.keys).sorted()[indexPath.section]
-            guard let item = parent.data[section]?[indexPath.row] else { fatalError("No item for IndexPath") }
-            cell.configure(with: { parent.content(item) })
+        return layout
+    }
+    
+    func makeDataSource(collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<CompositionalCollectionSection<ItemIdentifier>, ItemIdentifier> {
+        let dataSource = UICollectionViewDiffableDataSource<CompositionalCollectionSection<ItemIdentifier>, ItemIdentifier>(collectionView: collectionView) { collectionView, indexPath, item -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CompositionalCollectionCell<Content>.reuseIdentifier, for: indexPath) as? CompositionalCollectionCell<Content> else { return nil}
+            let view = self.content(indexPath, item)
+            cell.configure(with: view)
             return cell
         }
         
-        func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-            let section = Array(parent.data.keys)[indexPath.section]
-            
-            switch kind {
-            case UICollectionView.elementKindSectionHeader:
-                guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: parent.headerReuseIdentifier, for: indexPath) as? CollectionViewSectionHeaderFooter<Header> else { fatalError("Invalid view type") }
-                let headerView = parent.boundarySupplementaryItem(section, kind)
-                header.configure(with: headerView)
-                return header
-            default:
-                assert(false, "Invalid element type")
-            }
-        }
-        
-        var parent: UICollectionViewWrapper
-        var dataSource: UICollectionViewDiffableDataSource<Section, ItemIdentifier>?
-        
-        init(_ parent: UICollectionViewWrapper) {
-            self.parent = parent
-        }
-
-        func configureLayout() -> UICollectionViewLayout {
-            let layout  = UICollectionViewCompositionalLayout { (section: Int, environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection in
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.2))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(200))
-                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 3)
-
-                let headerFooterSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44))
-                let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerFooterSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
-//                header.pinToVisibleBounds = true
-                
-                let section = NSCollectionLayoutSection(group: group)
-                section.orthogonalScrollingBehavior = .groupPaging
-                section.interGroupSpacing = 24
-                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 16, trailing: 0)
-                section.boundarySupplementaryItems = [header]
-                
-                return section
-            }
-
-            return layout
-//            return UICollectionViewFlowLayout()
-        }
+        return dataSource
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    class Coordinator {
+        var dataSource: UICollectionViewDiffableDataSource<CompositionalCollectionSection<ItemIdentifier>, ItemIdentifier>?
     }
 }
+
+
+//struct UICollectionViewWrapper<SelectionValue, Content>: UIViewRepresentable where SelectionValue: Hashable, Content: View {
+//    let cellReuseIdentifier = "CollectionViewCell"
+//    let headerReuseIdentifier = "CollectionViewHeader"
+//
+//    var content: () -> Content
+//
+//    func makeUIView(context: Context) -> UICollectionView {
+//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout() )
+//
+//        collectionView.register(CollectionViewCell<Content>.self, forCellWithReuseIdentifier: cellReuseIdentifier)
+//
+//        collectionView.backgroundColor = .clear
+//
+//        return collectionView
+//    }
+//
+//    func updateUIView(_ uiView: UICollectionView, context: Context) {
+//
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator()
+//    }
+//
+//    class Coordinator: NSObject {
+//
+//    }
+//}
+
 
 class CollectionViewSectionHeaderFooter<Content: View>: UICollectionReusableView {
     var content: UIHostingController<Content>?
@@ -142,19 +121,35 @@ class CollectionViewSectionHeaderFooter<Content: View>: UICollectionReusableView
         addSubview(content.view)
         
         NSLayoutConstraint.activate([
-            content.view.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-            content.view.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
-            content.view.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
-            content.view.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+            content.view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            content.view.topAnchor.constraint(equalTo: topAnchor),
+            content.view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            content.view.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 }
 
-class CollectionViewCell<Content: View>: UICollectionViewCell {
+struct CompositionalCollectionSection<Item: Hashable>: Hashable {
+    static func == (lhs: CompositionalCollectionSection<Item>, rhs: CompositionalCollectionSection<Item>) -> Bool {
+        lhs.id == rhs.id && lhs.items == rhs.items
+    }
+
+    var id = UUID()
+    var items: [Item]
+    var layout: () -> NSCollectionLayoutSection
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(items)
+    }
+}
+
+class CompositionalCollectionCell<Content: View>: UICollectionViewCell {
+    static var reuseIdentifier: String { "CollectionViewCellType" }
     var content: UIHostingController<Content>?
     
-    func configure(with content: () -> Content) {
-        self.content = UIHostingController(rootView: content())
+    func configure(with content: Content) {
+        self.content = UIHostingController(rootView: content)
         configureHiearchy()
     }
     
@@ -167,12 +162,11 @@ class CollectionViewCell<Content: View>: UICollectionViewCell {
         contentView.addSubview(content.view)
         
         NSLayoutConstraint.activate([
-            content.view.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-            content.view.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-            content.view.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
-            content.view.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor)
+            content.view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            content.view.topAnchor.constraint(equalTo: contentView.topAnchor),
+            content.view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            content.view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
-    
-    
 }
+
