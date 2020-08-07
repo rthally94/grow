67d435e4f19b758895732b040d27397755863052
Ryan Thally <rthally94@gmail.com>
2020-08-07 12:37:55 -0400

    Moved to manual model definition

---
+++ Shared/Model/CareTaskType.swift
@@ -0,0 +1,9 @@
+//
+//  CareTaskType.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 8/6/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import Foundation
---
+++ Shared/Model/CoreData/CareTaskType+CoreDataProperties.swift
@@ -0,0 +1,43 @@
+//
+//  CareTaskType+CoreDataProperties.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 8/6/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CareTaskType {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CareTaskType> {
+        return NSFetchRequest<CareTaskType>(entityName: "CareTaskType")
+    }
+
+    @NSManaged public var color: UIColor?
+    @NSManaged public var icon: UIImage?
+    @NSManaged public var id: UUID?
+    @NSManaged public var name_: String?
+    @NSManaged public var tasks: NSSet?
+
+}
+
+// MARK: Generated accessors for tasks
+extension CareTaskType {
+
+    @objc(addTasksObject:)
+    @NSManaged public func addToTasks(_ value: CareTask)
+
+    @objc(removeTasksObject:)
+    @NSManaged public func removeFromTasks(_ value: CareTask)
+
+    @objc(addTasks:)
+    @NSManaged public func addToTasks(_ values: NSSet)
+
+    @objc(removeTasks:)
+    @NSManaged public func removeFromTasks(_ values: NSSet)
+
+}
