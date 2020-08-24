703b8914e4ab45a65b3c63a2daf0cb2bbb427100
Ryan Thally <rthally94@gmail.com>
2020-08-24 14:00:14 -0400

    Changed from WillChange to DidChange

--- Shared/Models/PlantStorage.swift
+++ Shared/Models/PlantStorage.swift
@@ -34,7 +34,7 @@ class PlantStorage: NSObject, ObservableObject {
 }
 
 extension PlantStorage: NSFetchedResultsControllerDelegate {
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         plants = allPlantsController.fetchedObjects?.compactMap(Plant.init) ?? []
     }
     
