0d00f44048a8cc4deec3598352483e3754c8bc5f
Ryan Thally <rthally94@gmail.com>
2020-08-25 09:23:00 -0400

    Added convenience init
    Allows for creating a managed object from a local model

--- Shared/Persistence/CareTaskMO+Extensions.swift
+++ Shared/Persistence/CareTaskMO+Extensions.swift
@@ -8,10 +8,20 @@
 
 import Foundation
 import CoreData
+
 extension CareTaskMO {
     static func allTasksFetchRequest() -> NSFetchRequest<CareTaskMO> {
         let request: NSFetchRequest<CareTaskMO> = CareTaskMO.fetchRequest()
         request.sortDescriptors = [ NSSortDescriptor(keyPath: \CareTaskMO.type, ascending: true) ]
         return request
+    }
+}
+
+extension CareTaskMO {
+    convenience init?(task: CareTask, context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.id = task.id
+        self.notes = task.notes
     }
 }
