66f636265cfa0e53b9bdf8babfe6da6346ad38eb
Ryan Thally <rthally94@gmail.com>
2020-08-31 10:45:37 -0400

    Initial commit
    Created CareTaskDetail view

---
+++ Shared/Views/CareTaskDetail/CareTaskDetail.swift
@@ -0,0 +1,21 @@
+//
+//  CareTaskDetail.swift
+//  Grow iOS
+//
+//  Created by Ryan Thally on 8/31/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct CareTaskDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CareTaskDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        CareTaskDetail()
+    }
+}
