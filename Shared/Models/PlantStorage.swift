8427b58ee3e70ef44fba26fb2c83fb25efcace88
Ryan Thally <rthally94@gmail.com>
2020-08-31 10:27:20 -0400

    Updated to use only Core Data Model

--- Shared/Models/PlantStorage.swift
+++ Shared/Models/PlantStorage.swift
@@ -14,10 +14,11 @@ class PlantStorage: NSObject, ObservableObject {
     private let context: NSManagedObjectContext
     private let allPlantsController: NSFetchedResultsController<PlantMO>
     
-    @Published var plants = [Plant]()
+    @Published var plants = [PlantMO]()
     
     init(context: NSManagedObjectContext) {
         self.context = context
+        
         self.allPlantsController = NSFetchedResultsController<PlantMO>(fetchRequest: PlantMO.allPlantsFetchRequest(), managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
         super.init()
@@ -26,7 +27,7 @@ class PlantStorage: NSObject, ObservableObject {
         
         do {
             try allPlantsController.performFetch()
-            plants = allPlantsController.fetchedObjects?.compactMap(Plant.init) ?? []
+            plants = allPlantsController.fetchedObjects ?? []
         } catch {
             print("Fetch Failed! | \(error.localizedDescription)")
         }
@@ -35,54 +36,66 @@ class PlantStorage: NSObject, ObservableObject {
 
 extension PlantStorage: NSFetchedResultsControllerDelegate {
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        plants = allPlantsController.fetchedObjects?.compactMap(Plant.init) ?? []
+        plants = allPlantsController.fetchedObjects ?? []
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         switch type {
         case .update:
-            print("Updated")
+            print("Updated plant")
         case.delete:
-            print("Deleted")
+            print("Deleted plant")
         case .insert:
-            print("Inserted")
+            print("Inserted plant")
         case .move:
-            print("Moved")
+            print("Moved plant")
         default:
-            print("Other")
+            print("Other plant")
         }
     }
 }
 
 extension PlantStorage: StorageCRUD {
-    func add(_ item: Plant) {
-        if !plants.contains(item) {
-            let plant = PlantMO(context: self.context)
-            plant.id = item.id
-            plant.name = item.name
-            plant.isFavorite = item.isFavorite
-            plant.plantingDate = item.plantingDate
-            plant.careTasks = []
-            
-            try? context.save()
+    @discardableResult func create() -> PlantMO {
+        let plantsWithNewNameRequest = PlantMO.allPlantsFetchRequest()
+        plantsWithNewNameRequest.predicate = NSPredicate(format: "name_ MATCHES %@", "My New Plant \\d*$")
+        
+        let newPlantCount: Int
+        do {
+            let plants = try context.fetch(plantsWithNewNameRequest)
+            newPlantCount = plants.count
+        } catch {
+            print(error)
+            newPlantCount = 0
         }
+//        let newPlantCount = try? plantsWithNewNameRequest.execute().count
+        
+        let plant = PlantMO(context: context)
+        plant.name = "My New Plant \(newPlantCount + 1)"
+        plant.isFavorite = false
+        plant.plantingDate = nil
+        plant.careTasks = []
+        
+        try? context.save()
+        return plant
     }
     
-    func update(_ item: Plant) {
-        if plants.contains(item), let object = allPlantsController.fetchedObjects?.first(where: {$0.id == item.id}) {
-            object.name = item.name
-            object.isFavorite = item.isFavorite
-            object.plantingDate = item.plantingDate
-            
-            try? context.save()
-        }
+    func update(_ item: PlantMO, name: String? = nil, isFavorite: Bool? = nil, plantingDate: Date? = nil, careTasks: Set<CareTaskMO>? = nil) {
+        guard item.managedObjectContext == context else { return }
+        
+        item.name = name ?? item.name
+        item.isFavorite = isFavorite ?? item.isFavorite
+        item.plantingDate = plantingDate ?? item.plantingDate
+        item.careTasks = careTasks ?? item.careTasks
+        
+        try? context.save()
     }
     
-    func remove(_ item: Plant) {
-        if plants.contains(item), let object = allPlantsController.fetchedObjects?.first(where: {$0.id == item.id}) {
-            context.delete(object)
-        }
+    func delete(_ item: PlantMO) {
+        context.delete(item)
+        
+        try? context.save()
     }
     
-    typealias Item = Plant
+    typealias Item = PlantMO
 }
